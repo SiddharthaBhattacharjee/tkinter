c4cf8d8b05f39de21dcabb0ce7fe5872b5812c70
ARNAV KAUSHIK <arnav22112003@gmail.com>
2022-01-21 19:12:23 +0530

    added new file

---
+++ day1/label.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+root.geometry("444x253")
+root.title("get a new label to my gui")
+
+title_label = Label(text = '''here goes the text for our gui label''', bg="red", fg="black",padx = 140, pady= 90, font=("cursive",10, "bold "), borderwidth=5,relief=SUNKEN)
+
+
+title_label.pack()
+root.mainloop()
