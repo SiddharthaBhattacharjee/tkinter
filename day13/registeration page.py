14422e44ffff5b8923a650fa48ddfed4f18c2de5
ARNAV KAUSHIK <arnav22112003@gmail.com>
2022-02-05 13:18:13 +0530

    database connectivity

--- day13/registeration page.py
+++ day13/registeration page.py
@@ -1,44 +1,75 @@
 from tkinter import *
-
+import tkinter.messagebox  as msg
+import mysql.connector as mysql
 root=Tk()
 root.title("Registration page")
 
+def database():
+    b=mysql.connect(host="localhost",user="root", password="123456")
+    cursor=b.cursor()
+    cursor.execute("create database registeration ;")
+    cursor.execute("use registeration")
+    cursor.execute("create table info (name char(10),dob date,username varchar(10),password int(10));")
+
+def sql():
+    Name=nam.get()
+    Dob=dob.get()
+    password=pas.get()
+    confirm=cpas.get()
+    if (Name=="" or Dob==""or password=="" or confirm==""):
+        msg.showinfo("error","all fields are required")
+    elif (password != confirm):
+        msg.showinfo("error","password and confirm password must be same")
+    else:
+        a=mysql.connect(host="localhost",user="root", password="123456",database="registeration")
+        cursor=a.cursor()
+        cursor.execute(f"insert into info values('{nam.get()}','{dob.get()}','{usnam.get()}','{pas.get()}');")
+        a.commit()
+        a.close()
+        msg.showinfo("Done","data added")
 def getval():
     print("submitted")
     with open("reg.txt","a") as f:
         f.write((f"name is {nam.get()}\n")),
-        f.write((f"Class is {cla.get()}\n")),
-        f.write((f"Roll number is {rol.get()}\n")),
-        f.write((f"Gender is {gen.get()}\n")),
+        f.write((f"dob is {dob.get()}\n")),
+        f.write((f"password is {pas.get()}\n")),
+        f.write((f"confirm password is {cpas.get()}\n")),
 
 Name=Label(root,text="Name")
 Name.grid(row=1,column=0)
+userName=Label(root,text="Username")
+userName.grid(row=2,column=0)
 
-roll_num=Label(root,text="Roll No")
-roll_num.grid(row=2,column=0)
+dob=Label(root,text="dob")
+dob.grid(row=3,column=0)
 
-Class=Label(root,text="Class")
-Class.grid(row=3,column=0)
+password=Label(root,text="Password")
+password.grid(row=4,column=0)
 
-gender=Label(root,text="Gender")
-gender.grid(row=4,column=0)
+cpass=Label(root,text="confirm password")
+cpass.grid(row=5,column=0)
 
 nameentry=StringVar()
-classentry=StringVar()
+usernameentry=StringVar()
+dobentry=StringVar()
 rollentry=StringVar()
 genderentry=StringVar()
 
 nam=Entry(root,textvariable=nameentry)
-cla=Entry(root,textvariable=classentry)
-rol=Entry(root,textvariable=rollentry)
-gen=Entry(root,textvariable=genderentry)
+usnam=Entry(root,textvariable=usernameentry)
+dob=Entry(root,textvariable=dobentry)
+pas=Entry(root,textvariable=rollentry)
+cpas=Entry(root,textvariable=genderentry)
 
 nam.grid(row=1,column=1)
-cla.grid(row=3,column=1)
-rol.grid(row=2,column=1)
-gen.grid(row=4,column=1)
+usnam.grid(row=2,column=1)
+dob.grid(row=3,column=1)
+pas.grid(row=4,column=1)
+cpas.grid(row=5,column=1)
 
-Button(root,text="submit",command=getval).grid(row=6,column=1)
+Button(root,text="submit",command=getval).grid(row=6,column=2,padx=10)
+Button(root,text="create db",command=database).grid(row=6,column=0)
+Button(root,text="add data",command=sql).grid(row=6,column=1)
 
 root.geometry("300x200")
 root.mainloop()
